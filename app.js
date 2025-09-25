d2e6cf613a1b244db29c2bec35c4a39d8f72a251
Shamira <shamiraxp10@gmail.com>
2025-09-25 10:38:22 -0300

    aplicaçao da primeira API

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+// app.js
 
+class CatFact {
+    // O construtor recebe o texto do fato
+    constructor(textoDoFato) {
+        // E armazena esse texto dentro da propriedade 'fact'
+        this.fact = textoDoFato;
+    }
+}
+
+// app.js (continuação)
+
+async function getCatFact() {
+    // 1. "Pescamos" os dados da URL. O `await` espera a pescaria terminar.
+    const response = await fetch('https://catfact.ninja/fact');
+
+    // 2. Pegamos o resultado da pesca e o transformamos em um objeto JavaScript que podemos usar.
+    const data = await response.json();
+
+    // 3. Usamos nosso molde (a classe CatFact) para criar um objeto organizado,
+    //    passando para ele apenas a informação que nos interessa: data.fact
+    const fatoDeGato = new CatFact(data.fact);
+
+    // 4. Devolvemos o objeto prontinho.
+    return fatoDeGato;
+}
