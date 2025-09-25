a4463016afd5682e20ab2748d24398c9adcb4b4d
SartoreTandih <tandarasartore@gmail.com>
2025-09-25 11:02:01 -0300

    atualização 3

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 
+class DogImage {
+    constructor(jsonData) {
+        this.imageUrl = jsonData.message;
+    }
+}
+
+
+function renderDogImage(dogPic) {
+    const dogContainer = document.getElementById('dog-container');
+    dogContainer.innerHTML = ''; // Limpa o conteúdo anterior
+
+    const imgElement = document.createElement('img');
+    imgElement.src = dogPic.imageUrl;
+    imgElement.alt = "Uma foto de um cachorro aleatório";
+    imgElement.style.maxWidth = '500px';
+
+    dogContainer.appendChild(imgElement);
+}
+
+
+function getDogImage() {
+    const apiUrl = 'https://dog.ceo/api/breeds/image/random';
+
+    fetch(apiUrl)
+        .then(response => {
+        
+            return response.json();
+        })
+        .then(data => {
+            
+            const dogPic = new DogImage(data);
+            renderDogImage(dogPic);
+        })
+        .catch(error => {
+            
+            console.error('Ocorreu um erro ao buscar a imagem:', error);
+        });
+}
+
+
+
+const newImageButton = document.getElementById('new-image-btn');
+
+
+newImageButton.addEventListener('click', getDogImage);
+
+
+getDogImage();
